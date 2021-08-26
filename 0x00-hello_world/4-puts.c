12cc4be3f4cf0dee2f040b6266804fcdc06598a6
chachi16 <chachifanos@gmail.com>
2021-08-26 02:36:05 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  *main - prints a string to the output
+ *Return: nothing
  */
 int main(void)
 {
 char Hello[] = "\"Programming is like building a multilingual puzzle";
- 
+
 puts(Hello);
 return (0);
 }
