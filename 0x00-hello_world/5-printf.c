76d81accc2836dd9b60ed3480c948a81af8389aa
chachi16 <chachifanos@gmail.com>
2021-08-26 03:01:28 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,8 @@
  *main - Prints a string
  *Return: Nothing
  */
-int main(void){
-printf("with proper grammar, but the outcome is a piece of art,");
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, ");
 return (0);
 }
