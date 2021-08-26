5bb95147d4f28d2a8b2cd47bb81ce1f60ce51ef9
chachi16 <chachifanos@gmail.com>
2021-08-26 03:30:43 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stido.h>
+/**
+ *main - Prints sizes of data types
+ *Return: Nothing
+ */
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of a int: %d byte(s)\n", sizeof(int));
+printf("Size of a char: %d byte(s)\n", sizeof(long int));
+printf("Size of a char: %d byte(s)\n", sizeof(long long int));
+printf("Size of a char: %d byte(s)\n", sizeof(float));
+return (0);
+}
