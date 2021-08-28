35f009a3693fd1e5a80d6e73cf14f0c92cd958e6
chachi16 <chachifanos@gmail.com>
2021-08-28 04:41:00 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - void function
+ *Return: nothing
+ */
+int main(void)
+{
+ int  n = 0 ;
+
+ while (n < 10)
+   {
+     putchar(n + '0');
+            n++;
+   }
+ return (0);
+}
