c1704abf11df9f34b4af69e7d0d0f4b4961a23d8
chachi16 <chachifanos@gmail.com>
2021-08-28 05:06:56 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - void function
+ *Return: nothing
+ */
+int main(void)
+{
+char b = 0x0
+int i = 0
+  
+while (i <= 15)
+{
+putchar('%x', b);
+b++;
+}
+putchar('\n')
+return (0);
