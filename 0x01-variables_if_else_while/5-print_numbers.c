d18455b88940a525fd659c93f033a72c9b948d27
chachi16 <chachifanos@gmail.com>
2021-08-28 04:26:47 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - void function
+ *Return: nothing
+ */
+int main(void)
+{
+int n = 0;
+
+while (n < 10)
+{
+printf("%d", n);
+n++;
+}
+printf("\n");
+return (0);
+}
