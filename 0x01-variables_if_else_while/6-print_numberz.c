7ee4870bc90f1bf4d7a15ab40bfbd4624a2c81c4
chachi16 <chachifanos@gmail.com>
2021-08-28 04:43:58 -0700

    Numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,12 +5,13 @@
  */
 int main(void)
 {
- int  n = 0 ;
-
- while (n < 10)
-   {
-     putchar(n + '0');
-            n++;
-   }
- return (0);
+int  n = 0 ;
+int s = \n
+while (n < 10)
+{
+putchar(n + '0');
+n++;
+}
+putcahr (s + '0');
+return (0);
 }
