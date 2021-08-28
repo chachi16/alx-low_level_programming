98321d60f5dc7e60a3b58de5ce2390fff892add3
chachi16 <chachifanos@gmail.com>
2021-08-28 03:57:02 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - void function
+ *Return: nothing
+ */
+int main(void)
+{
+char c = 'a';
+char s = '\n';
+
+while (c <= 'z')
+{
+putchar(c);
+if (c == e && c == q)
+continue;
+c++;
+}
+putchar(s)
+return (0);
+}
