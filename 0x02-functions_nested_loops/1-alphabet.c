cf501b532f403130cd03c37a4cf8bb83356b15eb
chachi16 <chachifanos@gmail.com>
2021-08-31 02:53:55 -0700

    alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,11 @@
  *print_alphabet_x10 - prints alphabet
  *return: 0
  */
-void print_alphabet_x10(void)
+void print_alphabet(void)
 {
 char alpha;
 alpha = 'a';
+
 while (alpha <= 'z')
 {
 _putchar(alpha);
