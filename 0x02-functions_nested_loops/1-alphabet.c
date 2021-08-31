c79ef3c73316318e9cb11c227a46e76f62f3d387
chachi16 <chachifanos@gmail.com>
2021-08-31 01:31:54 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_alphabet_x10 - prints alphabet
+ *return: 0
+ */
+void print_alphabet_x10(void)
+{
+char alpha;
+alpha = 'a';
+while (alpha <= 'z')
+{
+_putchar(alpha);
+alpha++;
+}
+ _putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,2 @@
-#include <stdio.h>
-#include <unistd.h>
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
+int _putchar(char c);
+void print_alphabet_x10(void);
