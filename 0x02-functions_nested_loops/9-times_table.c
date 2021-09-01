798533f392ac0444aff2c0e5efa76706dd5db799
chachi16 <chachifanos@gmail.com>
2021-09-01 03:53:15 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,36 +5,36 @@
  */
 void times_table(void)
 {
-int i, j, n, ld, fd;
+int a, b, mult;
 
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j < 10; j++)
-{
-n = i * j;
-if (n < 10)
-{
-if (j != 0)
-{
-_putchar(' ');
-}
-_putchar('0' + n);
-}
-else
-{
-ld = n % 10;
-fd = n / 10;
-_putchar('0' + fd);
-_putchar('0' + ld);
-}
-if (j == 9)
-{
-break;
-}
-_putchar(',');
-}
-_putchar(' ');
-}
-_putchar('\n');
-}
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 0 ; b <= 9 ; b++)
+		{
+			mult = a * b;
+
+			if (mult <= 9)
+			{
+				if (b != 0)
+				{
+					_putchar(' ');
+				}
+				_putchar(mult + '0');
+			}
+			else
+			{
+				_putchar(mult / 10 + '0');
+				_putchar(mult % 10 + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('\n');
+			}
+		}
+	}
 }
