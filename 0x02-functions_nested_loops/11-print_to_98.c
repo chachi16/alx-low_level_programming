8f6373dc01a3d916198b7cf306caf17a78fc5fde
chachi16 <chachifanos@gmail.com>
2021-09-01 04:22:50 -0700

    list

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,18 +10,30 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-while (n < 98)
+while (n <= 98)
 {
-printf("%d,", n );
+printf("%d", n );
+if (n == 98)
+{
+break;
+}
+printf(",");
 n++;
 }
+printf("\n");
 }
 else
 {
 while (n > 98)
 {
-printf("%d,", n );
+printf("%d", n );
+if (n == 98)
+{
+break;
+}
+printf(",");
 n--;
 }
+printf("\n");
 }
 }
