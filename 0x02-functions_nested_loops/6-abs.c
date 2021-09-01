db2221b331071ad7f6a38099207c8da489e77972
chachi16 <chachifanos@gmail.com>
2021-08-31 23:46:04 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -6,13 +6,12 @@
  */
 int _abs(int n)
 {
-  if (n <= 0)
-    {
-      _putchar(-n);
-    }
-  else
-    {
-    _putchar(n);
-    }
-return (0);
+if (n <= 0)
+{
+return(-n);
+}
+else
+{
+return(n);
+}
 }
