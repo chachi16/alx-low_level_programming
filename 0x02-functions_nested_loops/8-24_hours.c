f64c81499e761475d5566525c43a4f002c15b53f
chachi16 <chachifanos@gmail.com>
2021-09-01 00:38:41 -0700

    day time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *jack_bauer - prints every minute of the day
+ *Return: nothing
+ */
+void jack_bauer(void)
+{
+int i, j;
+
+for (i = 00 ; i < 25 ; i++)
+{
+for (j = 00 ; j < 60 ; j++)
+{
+_putchar('0' + i);
+_putchar(':');
+_putchar('0' + j);
+-putchar('\n');
+}
+}
