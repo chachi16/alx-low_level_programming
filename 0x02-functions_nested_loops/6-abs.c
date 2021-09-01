62fc697204c55aab2c4555f48d09c5c45db691a6
chachi16 <chachifanos@gmail.com>
2021-08-31 23:16:51 -0700

    absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,6 +2,7 @@
 /**
  *_abs - computes the absolute value of an integer
  *@n: the number to be computed
+ *Return: always zero
  */
 int _abs(int n)
 {
@@ -14,4 +15,5 @@ else
 n = -1 * n;
 _putchar(n + '0');
 }
+return (0);
 }
