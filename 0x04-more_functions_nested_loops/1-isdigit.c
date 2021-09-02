196a7a065309d6b7a60788c69940ef99ef82e04a
chachi16 <chachifanos@gmail.com>
2021-09-02 01:47:20 -0700

    addition and multiplication

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *_isdigit - checks for a digit
+ *@c: number to be checked
+ *return: 0 and 1
+ */
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *mul - multiplies two intigers
+ *@a: first operand
+ *@b: second operand
+ *return:result of multiplication
+ */
+int mul(int a, int b)
+{
+int result;
+
+result = a * b;
+return (result);
+}
