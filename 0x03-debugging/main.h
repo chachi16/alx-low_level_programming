b835c6004e3be281107688e229534f802d745fa6
chachi16 <chachifanos@gmail.com>
2021-09-01 23:29:02 -0700

    date

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-
+void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
