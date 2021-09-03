79ef5351a54a82240d7550cd7c2b1e4226d8c8ad
chachi16 <chachifanos@gmail.com>
2021-09-03 01:33:23 -0700

    correction on leap_year debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
+int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
