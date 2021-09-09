973e6c83446cedb59c6251bb3d0850249d137e75
chachi16 <chachifanos@gmail.com>
2021-09-09 01:11:29 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,7 +7,9 @@
  */
 void rev_string(char *s)
 {
-int i = 0;
+  int i, j;
+   i  = 0;
+   j = 0;
 
 while (s[i] > '\0')
 {
@@ -16,7 +18,12 @@ i++;
 i = i - 1;
 while (i >= 0)
 {
-*s = s[i];
+while (s[j] != '\0')
+{
+
+s[j] = s[i];
+j++;
+}
 i--;
 }
 _putchar('\n');
