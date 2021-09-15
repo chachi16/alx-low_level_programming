6718baec0b0587e49fde0a02371258ed6402d473
chachi16 <chachifanos@gmail.com>
2021-09-15 00:58:28 -0700

    all functions in 0x05 c

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+char *_strpbrk(char *s, char *accept)
+{
+	int i = 0;
+	int j;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				return (s + i);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,18 @@
+#iclude "main.h"
+/**
+ * print_diagsums - print the sum of a diagonals
+ * @a: the square matrix
+ *@size: size of the matrix
+ *
+ */
+void print_diagsums(int *a, int size)
+{
+	int b;
+	int sum = 0;
+	
+	for (b = 0; b =< size; b++)
+	{
+	 sum += a[b][b];
+	}
+	_putchar('0' + sum);
+}
