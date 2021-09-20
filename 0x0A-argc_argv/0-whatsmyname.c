1af0b84b62c9a975175be355dd0f7aaba1257ea2
chachi16 <chachifanos@gmail.com>
2021-09-20 03:31:28 -0700

    this function prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *main - prints the name of a program
+ *@argc: the length of argv
+ *@argv: array of strings
+ *
+ *Return: always 0
+ */
+int main(int argc, char* argv[])
+{
+_puthchar(argv[0]);
+_putchar(\n);
+return (0);
+}
