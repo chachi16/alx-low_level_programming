a63bd4cf12224f152ad97b464d60938c8910d437
chachi16 <chachifanos@gmail.com>
2021-09-21 04:32:27 -0700

    this function returns a pointer to a newlu allocated space in memory

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,35 +1,51 @@
-#include <stdlib.h>
 #include "main.h"
-#include <string.h>
 /**
- *_strdup - returns a newly allocated space in memory
- *@str: string parameter
+ * _strlen - give the length of a string
+ * @s: the string
  *
- *Return: pointer to a string
+ * Return: the length of a string
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
+}
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strdup - returns a pointer which contains a copy of the string
+ * given as a parameter
+ * @str: The string to print
+ *
+ *
+ * Return: a pointer to the duplicated string or NULL if insufficient
+ * memory was available
  */
 char *_strdup(char *str)
 {
-char *sttr;
-int i;
-int c;
+	int i;
+	char *copy;
 
-c =  strlen(str);
-if (str == NULL)
-{
-return (NULL);
-}
-else
-{ 
-sttr = malloc(sizeof(char) * c + 1);
-for (i = 0 ; str[i] != '\0' ; i++)
-{
-sttr[i] = str[i];
-}
-if (sttr == NULL)
-{
-return (NULL);
-}
-return (sttr);
-free(sttr);
-}
+	if (str == '\0')
+	{
+		return ('\0');
+	}
+	i = _strlen(str);
+	copy = malloc(sizeof(char) * i + 1);
+	if (copy == '\0')
+	{
+		return ('\0');
+	}
+	else
+	{
+		for (i = 0 ; str[i] != '\0' ; i++)
+		{
+			copy[i] = str[i];
+		}
+	}
+	copy[i] = '\0';
+	return (copy);
 }
