e038d8f898695b83033bd7f91ccaa882aeb90157
chachi16 <chachifanos@gmail.com>
2021-09-21 23:35:54 -0700

    this functiom frees a greed

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees 2 dimensional grid
+ * @grid: two dimensional grid
+ * @height: height of the array
+ *
+ * Return: a pointer to a 2 dimensional array of integers
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	if (grid == NULL || height <= 0)
+	{
+		return;
+	}
+
+	for (a = height - 1; a >= 0 ; a--)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}
