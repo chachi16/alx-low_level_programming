77f4702658e4e529501cfcedea6cea18a7440a96
chachi16 <chachifanos@gmail.com>
2021-09-21 23:29:45 -0700

    this function concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,80 +1,90 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- *_strlen - counts the length of a string
- *@s: string parameter
+ * _strlen - give the length of a string
+ * @s: the string
  *
- *Return: length of a string
+ * Return: the length of a string
  */
 int _strlen(char *s)
 {
-  int i = 0;
+	int i;
 
-  while (s[i] != '\0')
-    {
-      i++;}
-  return (i);
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
 }
+
 /**
- *_strcat - concatenates two strings
- *@dest: destination string parameter
- *@src: source string parameter
+ * _strcat - concatenates two strings
+ * @dest: input parameter string
+ * @src: input parameter string
  *
- *Return: string
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-  int i = 0;
+	int a;
+	int b;
 
-  while (dest[i] != '\0')
-    {
-      i++;
-    }
-  int j = 0;
+	a = 0;
 
-  while (src[j] != '\0')
-    {
-      dest[i] = src[j];
-      i++;
-      j++;
-    }
-  return (dest);
+	while (dest[a] != 0)
+	{
+		a++;
+	}
+
+	b = 0;
+
+	while (src[b] != 0)
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	return (dest);
 }
+
 /**
- *str_concat - concatenates two strings
- *@s1: string parameter
- *@s2: string parameter
+ * str_concat - concatenate to strings
+ * @s1: the string to print
+ * @s2: the string to print
  *
- *Return: pointer to a string
+ * Return: pointer that contains the content of s1 followed by s2
  */
 char *str_concat(char *s1, char *s2)
 {
-  char *new_string;
-  int length;
-  int i;
-  char *concatenated;
-  
-  i = 0;
-  concatenated = _strcat(s1, s2);
-  length = _strlen(s1) + _strlen(s2);
-  if (s1 == NULL && s2 == NULL)
-    {
-      return (NULL);
-    }
-  else
-    {
-      new_string = malloc(sizeof(char) * length +1);
+	int length;
+	char *space;
 
-      if (new_string == NULL)
+	if (s1 == NULL)
 	{
-	  return (NULL);
+		s1 = "";
 	}
-      while (i <= length)
+	if (s2 == NULL)
 	{
-	  new_string[i] = concatenated[i];
-		    i++;
+		s2 = "";
 	}
-    }
-  return (new_string);
-  free(new_string);
-}  
+
+	length = _strlen(s1) + _strlen(s2);
+	if (s1 != NULL && s2 != NULL)
+	{
+		space = malloc(sizeof(char) * length + 1);
+
+		if (space == NULL)
+		{
+			return ('\0');
+		}
+		else
+		{
+			space = _strcat(space, s1);
+			space = _strcat(space, s2);
+		}
+	}
+	else
+	{
+		space = "";
+	}
+	return (space);
+	free(space);
+}
