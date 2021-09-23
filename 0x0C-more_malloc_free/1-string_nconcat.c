82da4e4ba51d5a944a01381b55d7ba857e4c3be9
chachi16 <chachifanos@gmail.com>
2021-09-22 23:10:22 -0700

    this function concatenates two strings

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strlen - give the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+		;
+	return (i);
+}
+
+/**
+ *string_nconcat - concatenates two strings
+ *@s1: first string parameter
+ *@s2: second string parameter
+ *@n: first bytes of s2
+ *
+ *Return: pointer two a string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int length1, length2, i, j;
+	char *space;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	length1 = _strlen(s1);
+	length2 = _strlen(s2);
+
+	if (n >= length2)
+	{
+		n = length2;
+	}
+
+	space = malloc(sizeof(char) * (n + length1 + 1));
+
+	if (space == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0 ; i < length1 ; i++)
+		space[i] = s1[i];
+	for (j = 0 ; s2[j] != '\0' && j != n ; j++, i++)
+		space[i] = s2[j];
+
+	space[i] = '\0';
+	return (space);
+}
