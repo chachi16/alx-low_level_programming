d86d8bda11af1090fa82d02968f6a5f94f731a1e
chachi16 <chachifanos@gmail.com>
2021-10-07 00:21:32 -0700

    this function frees a list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list - frees a list
+ * @head: head of the linked list
+ *
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		free(head->str);
+		free_list(head->next);
+	}
+	free(head);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,9 @@
 #ifndef LIST_H
 #define LIST_H
+#include <string.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
